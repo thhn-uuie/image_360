2a754c739793441df66e43583bfa4482bcbcc81f
Thu Hien <117459480+thhn-uuie@users.noreply.github.com>
2023-07-03 13:23:37 +0700

    Update _form.php

--- backend/views/products/_form.php
+++ backend/views/products/_form.php
@@ -1,5 +1,7 @@
 <?php
 
+use common\models\base\Categories;
+use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
@@ -16,9 +18,22 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'description')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'status')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'status')->dropDownList(
+        [
+            'Hoạt động' => 'Hoạt động',
+            'Không hoạt động' => 'Không hoạt động',
+        ],
+        [
+            'prompt' => 'Trạng thái'
+        ]
+    ) ?>
 
-    <?= $form->field($model, 'id_category')->textInput() ?>
+    <?= $form->field($model, 'id_category')->dropDownList(
+        ArrayHelper::map(Categories::find()->all(),'id_category', 'name_category'),
+        [
+            'prompt' => 'Danh mục'
+        ]
+    ) ?>
 
     <?= $form->field($model, 'image')->textInput(['maxlength' => true]) ?>
 
@@ -26,13 +41,7 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'qr_code')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'created_at')->textInput() ?>
-
-    <?= $form->field($model, 'created_by')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'updated_at')->textInput() ?>
-
-    <?= $form->field($model, 'updated_by')->textInput(['maxlength' => true]) ?>
+    
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
