258ec819f0d72d90122b1dded7d2f5e643a3a190
ThuyLinh nekk <dothuylinh_t65@hus.edu.vn>
2023-08-02 13:12:15 +0700

    be

--- backend/views/users/view.php
+++ backend/views/users/view.php
@@ -17,7 +17,6 @@ $this->params['breadcrumbs'][] = $this->title;
     <?= Html::encode($this->title) ?>
   </h1>
 
-  <html>
 
   <head>
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
@@ -31,35 +30,37 @@ $this->params['breadcrumbs'][] = $this->title;
 
   <body>
     <form>
-    <h1 class="username"><?php echo $model->username ?></h1>
+      <h1 class="username">
+        <?php echo $model->username ?>
+      </h1>
       <div class="contentform">
-        <div id="sendmessage"> Your message has been sent successfully. Thank you. </div>
-
-
         <div class="leftcontact">
-          
           <div class="form-group">
             <p>E-mail </p>
-            <span class="icon-case"><i class="fa fa-envelope-open">  </i></span>
-            <div class="form-control" style="margin-left: 40px"><?php echo $model->email ?></div>
+            <span class="icon-case"><i class="fa fa-envelope-open"> </i></span>
+            <div class="form-control" style="margin-left: 40px">
+              <?php echo $model->email ?>
+            </div>
             <div class="validation"></div>
           </div>
 
           <div class="form-group">
             <p>Người tạo </p>
             <span class="icon-case"><i class="fa fa-user-circle"></i></span>
-            <div class="form-control"><?php echo $model->created_by ?></div>
+            <div class="form-control">
+              <?php echo $model->created_by ?>
+            </div>
             <div class="validation"></div>
           </div>
 
           <div class="form-group">
             <p>Người cập nhật </p>
             <span class="icon-case"><i class="fa fa-user-circle"></i></span>
-            <div class="form-control"><?php echo $model->updated_by ?></div>
-
+            <div class="form-control">
+              <?php echo $model->updated_by ?>
+            </div>
             <div class="validation"></div>
           </div>
-
         </div>
 
         <div class="rightcontact">
@@ -67,93 +68,42 @@ $this->params['breadcrumbs'][] = $this->title;
           <div class="form-group">
             <p>Chức năng </p>
             <span class="icon-case"><i class="fa fa-user"></i></span>
-            <div class="form-control"><?php echo $model->id_role ?></div>
+            <?php $roleName = Yii::$app->request->post('name'); ?>
+            <div class="form-control">
+              <?php if ($model->id_role == 1) {
+                echo 'admin';
+              } else if ($model->id_role == 2) {
+                echo 'user';
+              } ?>
+            </div>
             <div class="validation"></div>
           </div>
 
           <div class="form-group">
             <p>Thời gian tạo </p>
             <span class="icon-case"><i class="fa fa-clock"></i></span>
-            <div class="form-control"><?php echo $model->created_at ?></div>
+            <div class="form-control">
+              <?php echo $model->created_at ?>
+            </div>
             <div class="validation"></div>
           </div>
 
           <div class="form-group">
             <p>Thời gian cập nhật </p>
             <span class="icon-case"><i class="fa fa-clock"></i></span>
-            <div class="form-control"><?php echo $model->updated_at ?></div>
-            <div class="validation"></div>
-          </div>
-
-        </div>
-      </div>
-
-    </form>
-
-
-  </body>
-
-  </html>
-
-
-
-
-
-
-
-
-
-
-  <!-- <body>
-  <div class="testbox">
-    <form class="form2">
-      <fieldset>
-        <legend>
-          <?php echo $model->username ?>
-        </legend>
-        <div class="colums">
-          <div class="item">
-            <label for="fname" style="margin-right:10px;"> Email: </label>
-            <div>
-              <?php echo $model->email ?>
-            </div>
-          </div>
-          <div class="item">
-            <label for="lname" style="margin-right:10px; margin-left: 25px;"> Chức năng: </label>
-            <div>
-              <?php echo $model->id_role ?>
-            </div>
-          </div>
-          <div class="item">
-            <label for="fname" style="margin-right:10px;"> Thời gian tạo: </label>
-            <div>
-              <?php echo $model->created_at ?>
-            </div>
-          </div>
-          <div class="item">
-            <label for="lname" style="margin-right:10px;margin-left: 25px;"> Người tạo: </label>
-            <div>
-              <?php echo $model->created_by ?>
-            </div>
-          </div>
-          <div class="item">
-            <label for="address1" style="margin-right:10px;"> Thời gian cập nhật: </label>
-            <div>
+            <div class="form-control">
               <?php echo $model->updated_at ?>
             </div>
-          </div>
-          <div class="item">
-            <label for="address2" style="margin-right:10px;margin-left: 25px;">Người cập nhật: </label>
-            <div>
-              <?php echo $model->created_by ?>
-            </div>
-          </div>
-          <div class="question">
+            <div class="validation"></div>
           </div>
         </div>
-  </div>
-</div>
-</body> -->
+      </div>
+    </form>
+  </body>
+
+
+
+
 
   <div style="text-align: center;">
 
@@ -171,15 +121,17 @@ $this->params['breadcrumbs'][] = $this->title;
 
 
   <style>
-    .form-control{
+    .form-control {
       margin-left: 40px;
-    width: 69%;
+      width: 69%;
     }
+
     .username {
-  font-size: 36px;
-  font-weight: bold;
-  color: #0a4d37;
-}
+      font-size: 36px;
+      font-weight: bold;
+      color: #0a4d37;
+    }
+
     body {
       margin: auto;
       background: #eaeaea;
@@ -342,137 +294,3 @@ $this->params['breadcrumbs'][] = $this->title;
       display: block;
     }
   </style>
-  <!-- 
-<style>
-  .colums {
-    display: flex;
-    justify-content: space-between;
-    flex-direction: row;
-    flex-wrap: wrap;
-  }
-
-  .colums div {
-    width: 48%;
-  }
-
-  legend {
-    font-size: 70px;
-    color: inherit;
-    font-family: Roboto;
-    font-weight: bold;
-    line-height: -0.8;
-    letter-spacing: 2px;
-  }
-
-  legend {
-    display: block;
-    width: 90%;
-    max-width: 106%;
-    padding: -11px;
-    margin-top: -0.9rem;
-
-    margin-bottom: -0.5rem;
-    font-size: 3.5rem;
-    line-height: inherit;
-    color: inherit;
-    white-space: normal;
-  }
-
-  .dropdown-item {
-    width: 100%;
-    padding: 11px 20px;
-  }
-
-  form {
-    width: 100%;
-    height: 48px;
-    padding: -20px;
-    background: #fdfdfd;
-    box-shadow: 3 0 8px #073315;
-  }
-
-  .form2 {
-    width: 100%;
-    height: 180px;
-    padding: -20px;
-    background: #fdfdfd;
-    box-shadow: 3 0 8px #073315;
-  }
-
-  .btn-primary {
-    width: 150px;
-    padding: 10px;
-    border: none;
-    border-radius: 4px;
-    font-size: 20px;
-    color: white;
-    cursor: pointer;
-    text-align: center;
-    text-decoration: none;
-    display: inline-block;
-    transition-duration: 0.1s;
-    background-color: #cff2e9;
-    font-family: Roboto;
-
-  }
-
-  .btn-danger {
-    width: 150px;
-    padding: 10px;
-    border: none;
-    border-radius: 4px;
-    font-size: 20px;
-    color: white;
-    cursor: pointer;
-    text-align: center;
-    text-decoration: none;
-    display: inline-block;
-    transition-duration: 0.1s;
-    background-color: #e78e96;
-    font-family: Roboto;
-
-  }
-
-  fieldset {
-    border: none;
-    margin: 16px;
-    padding: 0;
-  }
-
-  .btn-primary:hover {
-    background-color: #2f9345;
-  }
-
-  .btn-primary:active {
-    box-shadow: 0 0 9e9q inset #0009;
-    background-color: black;
-    outline: none;
-  }
-
-  .btn-danger:hover {
-    background-color: #d73b49;
-  }
-
-  .btn-danger:active {
-    box-shadow: 0 0 2e2q inset #0009;
-    background-color: black;
-    outline: none;
-  }
-
-  .card {
-    width: 627px;
-    height: 200px;
-  }
-
-  body {
-    background: #eee;
-  }
-
-  .bg-white {
-    background: #d6f4ef !important;
-    border: 1px solid #a31b1b !important;
-    color: #083c73;
-    margin-left: 10px;
-  }
-  
-</style> -->
